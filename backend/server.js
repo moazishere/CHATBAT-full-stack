99ea8b23549c084e05749133d6a5a20f317abacd
moaz <moazbahra10@gmail.com>
2025-10-29 22:55:23 +0300

    uu

--- backend/server.js
+++ backend/server.js
@@ -14,7 +14,7 @@ app.use(express.json({ limit: '50mb' }))
 app.use(express.urlencoded({ extended: true, limit: '50mb' }))
 app.use(cookieParser())
 app.use(cors({
-    origin: "http://localhost:5173",
+    origin: ["http://localhost:5173", "https://chatbat-five.vercel.app"],
     credentials: true
 }
 ))
